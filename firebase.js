f54bd35369208c2b998fa65e1b2a8735ce497e8d
José Vargas <josevargas@MacBook-Air-de-Jose-3.local>
2022-08-09 16:11:48 -0400

    firebase

--- firebase.js
+++ firebase.js
@@ -5,14 +5,21 @@ import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqVtvYengYuutWowX4Y3y8Xid9d1CnTCI",
+  apiKey: "AIzaSyBrjqCLVrLmVqTF-DUGtvIMhuuZXtF-r2o",
+  authDomain: "wefinder-7276a.firebaseapp.com",
+  projectId: "wefinder-7276a",
+  storageBucket: "wefinder-7276a.appspot.com",
+  messagingSenderId: "331599924871",
+  appId: "1:331599924871:web:b6c4d6c627ee6fb710b222"
+};
+/*
+apiKey: "AIzaSyDqVtvYengYuutWowX4Y3y8Xid9d1CnTCI",
   authDomain: "wefinder-app.firebaseapp.com",
   projectId: "wefinder-app",
   storageBucket: "wefinder-app.appspot.com",
   messagingSenderId: "46096713211",
   appId: "1:46096713211:web:0754cd61c8153b0bdab75e"
-};
-
+*/
 //Initialize Firebase
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
